d8f7117619b6cfe874cbbca2e39fe2b2e52a6da8
bykhov <bykhov@users.noreply.github.com>
2024-03-04 07:02:25 +0000

    Deploying to gh-pages from @ bykhov/bykhov.github.io@dfbc7c93e4a3a3a5ee8437d2be56ae9380ed08fe 🚀

--- suppl/rp/code/acf/acf_fir_example.m
+++ suppl/rp/code/acf/acf_fir_example.m
@@ -1,8 +1,11 @@
+close all
+clear all
+
 N = 1e5;    % Signal length
 sigma = 1;  % Noise variance
 w = sigma*randn(1,N);   % Noise realization
 y = filter([1/2 1/2],1,w);  % Filtered noise
-figure(1)
+fig1 = figure(1);
 [c, lg] = xcorr(y,5,'biased');    % Auto-covariance C_x[k], for |k| <= 5
 stem(lg,c)
 
@@ -12,18 +15,31 @@ title('$R_X[k]$',Interpreter='latex')
 set(gca,'YTick',[0 sigma^2/4 sigma^2/2])
 %ylim([-0.5 sigma^2/2*1.2])
 grid on
-set(findobj(fig,'-property','FontName'), ...
+set(findobj(fig1,'-property','FontName'), ...
     'FontName','Times New Roman','FontSize',  13)
 
 %%
-figure(2)
+fig2 = figure(2);
 nexttile
 plot(w(1:100),'LineWidth',1)
 title('Noise')
+xlabel('Time')
 grid on
+ylim([-2,2])
 nexttile
-plot(x(1:100),'LineWidth',1)
+plot(y(1:100),'LineWidth',1)
+xlabel('Time')
 title('Signal')
 grid on
-set(findobj(fig,'-property','FontName'), ...
+ylim([-2,2])
+set(findobj(fig2,'-property','FontName'), ...
     'FontName','Times New Roman','FontSize',  13)
+
+%%
+fig3 = figure(3);
+z = filter([1/4 1/4 1/4 1/4],1,w);  % Filtered noise
+plot(z(1:100),'LineWidth',1)
+ylim([-2,2])
+grid on
+xlabel('Time')
+title('h=[1/4 1/4 1/4 1/4]')
