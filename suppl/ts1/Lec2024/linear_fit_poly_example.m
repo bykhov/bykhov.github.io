0f49abb10b68b76c3b3614db4b7ad76592060d58
Dima Bykhovsky <bykhov@users.noreply.github.com>
2024-06-17 19:53:25 +0300

    Update linear_fit_poly_example.m

--- suppl/ts1/Lec2024/linear_fit_poly_example.m
+++ suppl/ts1/Lec2024/linear_fit_poly_example.m
@@ -1,13 +1,13 @@
-clear all
+clearvars
 close all
 
 rng(234)
-M_train = 10;
+M_train = 100;
 sigma = 0.1;
 x_train = random('Uniform',0,0.6,M_train,1);
 p_coeff = [0.1 3 1 0.2 0.01];
 y_theory_train = polyval(p_coeff,x_train);
-y_train = y_theory_train  + sigma*randn(M_train,1); %y = x1 + 3*x1.^2 + 0.1*x1.^3
+y_train = y_theory_train  + sigma*randn(M_train,1); 
 
 M_test = 100;
 x_test = random('Uniform',0,0.6,M_test,1);
@@ -34,20 +34,26 @@ function yh = polynomial_regression_values(x,w)
     end
     yh = X*w;
 end
+
 %%
+N = 6;
+% Learning weights
+[~, w] = polynomial_regression_weights(x_train,y_train,N);
+
+% Test performance
+yh_test = polynomial_regression_values(x_test ,w);
+mse_N = norm(yh_test - y_test)^2/M_test;
+
+% For plots only
+x_theory = (0:.001:0.6)';
+yh = polynomial_regression_values(x_theory ,w);
 
 figure(1)
-N = 7;
-
-[~, w] = polynomial_regression_weights(x_train,y_train,N);
-x_theory = 0:.001:0.6;
-yh = polynomial_regression_values(x_theory' ,w);
-    
 plot(x_train,y_train,'o','MarkerSize',5,'MarkerFaceColor','r')
 hold on
 plot(x_test,y_test,'o','MarkerSize',2)
 plot(x_theory,yh,'-','LineWidth',1.25,'Color','k')
 hold off
 ylim([0 2])
-title(['N=' num2str(N)],'Interpreter','latex')
+title(['N=' num2str(N) ', mse=' num2str(mse_N,5)],'Interpreter','latex')
 grid on
